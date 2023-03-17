6381c0bc56e32bd88f91a1b25153df4a204f5f85
TamasPinter <119883462+TamasPinter@users.noreply.github.com>
2023-03-17 19:08:29 -0400

    started database files, high score schemas done 1-4

---
+++ server/models/HighScore1.js
@@ -0,0 +1,28 @@
+const { Schema, Types } = require('mongoose');
+
+const HighScoreSchema1 = new Schema(
+    {
+       user_id: {
+        type: Types.ObjectId,
+        ref: 'User',
+        required: true,
+       },
+       game_id: {
+        type: Types.ObjectId,
+        ref: 'Game',
+        required: true,
+       },
+       score: {
+        type: Number,
+        required: true,
+       },
+    },
+    {
+        toJSON: {
+            getters: true,
+        },
+        id: false,
+    }
+)
+
+module.exports = HighScoreSchema1;
